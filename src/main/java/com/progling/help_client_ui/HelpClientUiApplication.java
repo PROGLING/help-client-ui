18125ef115c141fcac9aec612a448861a0f93d67
jg <ella.code.j@gmail.com>
2025-07-16 14:35:05 +0900

    [INIT] 프로젝트 초기 셋팅

---
+++ src/main/java/com/progling/help_client_ui/HelpClientUiApplication.java
@@ -0,0 +1,13 @@
+package com.progling.help_client_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpClientUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpClientUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progling/help_client_ui/HelpClientUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progling.help_client_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpClientUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
